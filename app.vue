36172fc704ff7e043b1cb61764e879ec5f76eb68
kyletreptow <treptow@apollointeractive.com>
2021-04-11 02:46:28 -0700

    error passing

--- app.vue
+++ app.vue
@@ -10,23 +10,27 @@
         <main>
           <div class="panel" :class="{ before: step > 1, active: step == 1, after: step < 1 }">
             <h1>Step 1</h1>
-            <TextControl label="First Name" inputName="first_name" placeholder="John" />
-            <TextControl label="Last Name" inputName="last_name" placeholder="Doe" />
+            <TextControl label="First Name" inputName="first_name" placeholder="John" panel="1" />
+            <TextControl label="Last Name" inputName="last_name" placeholder="Doe" panel="1" />
             <button type="button" @click="step = 2">Next</button>
+            <div class="well">Errors: {{ errors[1] }}</div>
           </div>
           <div class="panel" :class="{ before: step > 2, active: step == 2, after: step < 2 }">
             <h1>Step 2</h1>
-            <RadioControl label="Car Make" inputName="car_make" :options="['Toyota', 'Ford', 'Chevy']" />
+            <RadioControl label="Car Make" inputName="car_make" :options="['Toyota', 'Ford', 'Chevy']" panel="2" />
             <button type="button" @click="step = 3">Next</button>
+            <div class="well">Errors: {{ errors[2] }}</div>
           </div>
           <div class="panel" :class="{ before: step > 3, active: step == 3, after: step < 3 }">
             <h1>Step 3</h1>
-            <CheckboxControl label="Colors" inputName="colors" :options="['Red', 'Blue', 'Green']" />
+            <CheckboxControl label="Colors" inputName="colors" :options="['Red', 'Blue', 'Green']" panel="3" />
             <button type="button" @click="step = 4">Next</button>
+            <div class="well">Errors: {{ errors[3] }}</div>
           </div>
           <div class="panel" :class="{ before: step > 4, active: step == 4, after: step < 4 }">
             <h1>Step 4</h1>
-            <SelectControl label="Shape" inputName="shape" :options="['Square', 'Circle', 'Triangle']" />
+            <SelectControl label="Shape" inputName="shape" :options="['Square', 'Circle', 'Triangle']" panel="4" />
+            <div class="well">Errors: {{ errors[4] }}</div>
           </div>
         </main>
       </form>
@@ -43,7 +47,23 @@
       components: { TextControl, RadioControl, CheckboxControl, SelectControl },
       data () {
         return {
-          step: 1
+          step: 1,
+          errors: {
+            1: [], 2: [], 3: [], 4: []
+          }
+        }
+      },
+      methods: {
+        addErrorFlag (flag, panel) {
+          if(!this.errors[panel].includes(flag)){
+            this.errors[panel].push(flag)
+          }
+        },
+        removeErrorFlag (flag, panel) {
+          let filtered = this.errors[panel].filter(function(value){
+            return value != flag;
+          })
+          this.errors[panel] = filtered
         }
       }
     }
--- components/controls/_control.vue
+++ components/controls/_control.vue
@@ -1,14 +1,34 @@
 <script>
   export default {
-    props: ['label', 'inputName', 'placeholder', 'options'],
-    data (){
+    props: ['label', 'inputName', 'placeholder', 'options', 'panel'],
+    data () {
       return {
         value: ''
       }
     },
     computed: {
-      valid: function(){
+      valid () {
         return this.value.length ? true : false
+      }
+    },
+    watch: {
+      value () {
+        if(this.valid) {
+          // call parent fn to add inputName
+          this.$parent.removeErrorFlag(this.inputName, this.panel)
+        } else {
+          // call parent fn to remove inputName
+          this.$parent.addErrorFlag(this.inputName, this.panel)
+        }
+      }
+    },
+    mounted () {
+      if(this.valid) {
+        // call parent fn to add inputName
+        this.$parent.removeErrorFlag(this.inputName, this.panel)
+      } else {
+        // call parent fn to remove inputName
+        this.$parent.addErrorFlag(this.inputName, this.panel)
       }
     }
   }
