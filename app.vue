90a5e16577cb6a8baa18f1b142a9ed444645b1bd
kyletreptow <treptow@apollointeractive.com>
2021-06-12 10:48:29 -0700

    remove data from view

--- app.vue
+++ app.vue
@@ -2,37 +2,25 @@
     <div class="wrap">
       <form class="" action="/" method="post" autocomplete="off">
         <header>
-          <button type="button" :class="{ active: step == 1 }" @click="step = 1">1</button>
-          <button type="button" :class="{ active: step == 2 }" @click="step = 2">2</button>
-          <button type="button" :class="{ active: step == 3 }" @click="step = 3">3</button>
-          <button type="button" :class="{ active: step == 4 }" @click="step = 4">4</button>
+          <button v-for="(i, index) in panels" :key="index+'_button'"
+          type="button" :class="{ active: step == index }" @click="step = index">{{ index + 1 }}</button>
         </header>
         <main>
-          <div class="panel" :class="{ before: step > 1, active: step == 1, after: step < 1 }">
-            <h1>Step 1</h1>
-            <TextControl label="First Name" inputName="first_name" placeholder="John" panel="1" />
-            <TextControl label="Last Name" inputName="last_name" placeholder="Doe" panel="1" />
-            <button type="button" @click="step = 2">Next</button>
-            <div class="well">Errors: {{ errors[1] }}</div>
-          </div>
-          <div class="panel" :class="{ before: step > 2, active: step == 2, after: step < 2 }">
-            <h1>Step 2</h1>
-            <RadioControl label="Car Make" inputName="car_make" :options="['Toyota', 'Ford', 'Chevy']" panel="2" />
-            <RadioControl label="School" inputName="school" :options="['CSUN', 'USC', 'UCLA', 'Other']" panel="2" />
-            <button type="button" @click="step = 3">Next</button>
-            <div class="well">Errors: {{ errors[2] }}</div>
-          </div>
-          <div class="panel" :class="{ before: step > 3, active: step == 3, after: step < 3 }">
-            <h1>Step 3</h1>
-            <CheckboxControl label="Colors" inputName="colors" :options="['Red', 'Blue', 'Green']" panel="3" />
-            <TextControl label="Nickname" inputName="nickname" placeholder="Chester" panel="3" />
-            <button type="button" @click="step = 4">Next</button>
-            <div class="well">Errors: {{ errors[3] }}</div>
-          </div>
-          <div class="panel" :class="{ before: step > 4, active: step == 4, after: step < 4 }">
-            <h1>Step 4</h1>
-            <SelectControl label="Shape" inputName="shape" :options="['Square', 'Circle', 'Triangle']" panel="4" />
-            <div class="well">Errors: {{ errors[4] }}</div>
+          <div v-for="(i, index) in panels" :key="index+'_panel'"
+          class="panel" :class="{ before: step > index, active: step == index, after: step < index }">
+            <h1>{{ i.title }}</h1>
+            <div v-for="input in i.inputs" :key="input.inputName">
+              <TextControl v-if="input.type == 'text'"
+              :label="input.label" :inputName="input.inputName" :placeholder="input.placeholder" :panel="index + 1" />
+              <RadioControl v-if="input.type == 'radio'"
+              :label="input.label" :inputName="input.inputName" :options="input.options" :panel="index + 1" />
+              <CheckboxControl v-if="input.type == 'checkbox'"
+              :label="input.label" :inputName="input.inputName" :options="input.options" :panel="index + 1" />
+              <SelectControl v-if="input.type == 'select'"
+              :label="input.label" :inputName="input.inputName" :options="input.options" :panel="index + 1" />
+            </div>
+            <button type="button" @click="step = index+2">Next</button>
+            <div class="well">Errors: {{ errors[index+1] }}</div>
           </div>
         </main>
       </form>
@@ -49,7 +37,71 @@
       components: { TextControl, RadioControl, CheckboxControl, SelectControl },
       data () {
         return {
-          step: 1,
+          panels: [
+            {
+              title: 'step 1',
+              inputs: [
+                {
+                  type: 'text',
+                  label: 'First Name',
+                  inputName: 'first_name',
+                  placeholder: 'John'
+                },
+                {
+                  type: 'text',
+                  label: 'Last Name',
+                  inputName: 'last_name',
+                  placeholder: 'Doe'
+                }
+              ]
+            },
+            {
+              title: 'step 2',
+              inputs: [
+                {
+                  type: 'radio',
+                  label: 'Car Make',
+                  inputName: 'car_name',
+                  options: ['Toyota', 'Ford', 'Chevy']
+                },
+                {
+                  type: 'radio',
+                  label: 'School',
+                  inputName: 'school',
+                  options: ['CSUN', 'USC', 'UCLA', 'Other']
+                }
+              ]
+            },
+            {
+              title: 'step 3',
+              inputs: [
+                {
+                  type: 'checkbox',
+                  label: 'Colors',
+                  inputName: 'colors',
+                  options: ['Red', 'Blue', 'Green']
+                },
+                {
+                  type: 'text',
+                  label: 'Nickname',
+                  inputName: 'nickname',
+                  placeholder: 'Chester'
+                }
+              ]
+            },
+            {
+              title: 'step 4',
+              inputs: [
+                {
+                  type: 'select',
+                  label: 'Shape',
+                  inputName: 'shape',
+                  options: ['Square', 'Circle', 'Triangle']
+                },
+              ]
+            }
+          ],
+          step: 0,
           errors: {
             1: [], 2: [], 3: [], 4: []
           }
